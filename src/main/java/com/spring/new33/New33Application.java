2d93ab0e97ea6537b00df33bb6a4875e7b6bc7a0
riadh_jr <riadh.j@db-consulting-group.com>
2022-04-23 13:23:48 +0100

    create entities from database

---
+++ src/main/java/com/spring/new33/New33Application.java
@@ -0,0 +1,13 @@
+package com.spring.new33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(New33Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/new33/New33ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.new33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class New33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
